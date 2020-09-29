d1afb0bdda339d509b691727b6ec017679bb41ce
David Larsen <davidplarsen@gmail.com>
2020-09-29 15:46:57 -0500

    Complete exercise 3

--- js-app/main.js
+++ js-app/main.js
@@ -4,10 +4,22 @@ const button = document.querySelector("#run-button");
 button.addEventListener("click", () => {
     getAllBeanVarieties()
         .then(beanVarieties => {
-            console.log(beanVarieties);
+            const beans = document.getElementById("beans")
+            beans.innerHTML = renderBeanHTML(beanVarieties)
         })
-});
+})
 
 function getAllBeanVarieties() {
     return fetch(url).then(resp => resp.json());
+}
+
+const renderBeanHTML = (beans) => {
+    htmlToRender = `<h2>Coffee Bean Varieties</h2>
+                    <ul>`
+    beans.forEach(bean => {
+        if (!bean.notes) bean.notes = "No additional notes"
+        htmlToRender += `<li><strong>${bean.name}, ${bean.region}:</strong> ${bean.notes}</li>`
+    })
+    htmlToRender += "</ul>"
+    return htmlToRender
 }
